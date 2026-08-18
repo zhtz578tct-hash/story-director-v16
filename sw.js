6eabfbde01bc429af04595eb570b52b5d915ac59
Grijesh Singh <zhtz578tct@privaterelay.appleid.com>
2026-08-18 17:23:48 +0530

    Upgrade service worker to sdv16-v2
    Updated service worker to version 2 with cache management.

--- sw.js
+++ sw.js
@@ -1,3 +1,30 @@
-const CACHE="sdv16-v1";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));
-self.addEventListener("fetch",e=>{if(e.request.method==="GET"&&new URL(e.request.url).origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE="sdv16-v2";
+
+self.addEventListener("install",e=>
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c=>c.addAll(["./","./index.html","./manifest.json"]))
+      .then(()=>self.skipWaiting())
+  )
+);
+
+self.addEventListener("activate",e=>
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(
+        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+      ))
+      .then(()=>self.clients.claim())
+  )
+);
+
+self.addEventListener("fetch",e=>{
+  if(
+    e.request.method==="GET" &&
+    new URL(e.request.url).origin===location.origin
+  ){
+    e.respondWith(
+      caches.match(e.request).then(r=>r||fetch(e.request))
+    );
+  }
+});
